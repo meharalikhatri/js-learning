79fdaf2c19525d0a1d5ef6b2077f1cfd2f281af5
meharalikhatri <56135432+meharalikhatri@users.noreply.github.com>
2024-03-19 09:02:56 +0000

    update till datatype

---
+++ 01_basics_java/datatype.js
@@ -0,0 +1,18 @@
+let name = "mehar ali khatri"
+let age = 40
+let isLoggedIn = true
+let temporature = null /* null is an object */
+let state;
+
+// number => 2 to power 53
+// bigint => it will be use in stock or in big numbers
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => find the uniqueness
+
+
+// Object
+
+console.log(typeof temporature);

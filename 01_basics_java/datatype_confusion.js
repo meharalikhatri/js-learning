9d4f22e5ed5c7823add3805a6747fcc17df7f481
meharalikhatri <56135432+meharalikhatri@users.noreply.github.com>
2024-03-19 09:25:53 +0000

    update confusion

--- 01_basics_java/datatype_confusion.js
+++ 01_basics_java/datatype_confusion.js
@@ -5,3 +5,14 @@ console.log(typeof age);
 let isNumber = Number(age)
 console.log(typeof isNumber);
 console.log(isNumber); /* this will return to you NaN error */
+
+let isValue = null
+console.log(isValue);
+console.log(typeof isValue);
+
+let convert_value = Number(isValue)
+console.log(convert_value);
+console.log(typeof convert_value)
+// "33" => 33
+// "33asd" => NaN
+// ture => 1 or false => 0

2d1237b6fe6c4b43db9d317958ed6b560e007732
meharalikhatri <56135432+meharalikhatri@users.noreply.github.com>
2024-03-22 18:08:47 +0000

    update till strings

---
+++ 01_basics_java/strings.js
@@ -0,0 +1,10 @@
+let str1 = "hello"
+let str2 = " mehar"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+console.log("1" + 2);
+console.log(2 + "1");
+console.log("1" + "1" + 2);
+console.log((5 + 1 + "5" + "2")*1 + 2);

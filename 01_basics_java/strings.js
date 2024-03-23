237a6d291aa702734ead53a444068dbc17ea2f97
meharalikhatri <56135432+meharalikhatri@users.noreply.github.com>
2024-03-23 17:19:07 +0000

    str

--- 01_basics_java/strings.js
+++ 01_basics_java/strings.js
@@ -4,13 +4,21 @@ let str3 = str1 + str2
 
 // console.log(str3);
 
-console.log("1" + 2);
-console.log(2 + "1");
-console.log("1" + "1" + 2);
-console.log((5 + 1 + "5" + "2")*1 + 2);
+// console.log("1" + 2);
+// console.log(2 + "1");
+// console.log("1" + "1" + 2);
+// console.log((5 + 1 + "5" + "2")*1 + 2);
 
 
 /* Use Backticks Method */
 const name = "mehar"
 const age = 40
-console.log ('my name is ${name} and my age is ${age}');
+// console.log ('my name is ${name} and my age is ${age}');
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//prototype
+
+let user_1 = "mehar"
+
+console.log(user_1)

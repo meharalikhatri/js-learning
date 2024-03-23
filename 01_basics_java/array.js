816d7b542dfc354eb636613134abdc20fd3680c7
meharalikhatri <56135432+meharalikhatri@users.noreply.github.com>
2024-03-23 19:19:40 +0000

    update till array

---
+++ 01_basics_java/array.js
@@ -0,0 +1,12 @@
+//Array
+//Array will starts will square brackets []
+// these are parantheses () , square brackets [] , curly brases {}
+
+const myArray = [0,1,2,3,4,5] /* inside data calls eliments */
+const myarr2 = ["Ali","Mehar","Khatri"]
+const newArray = new Array(100,10,501,1002)
+
+console.log(myArray);
+console.log(myarr2);
+console.log(newArray);
+console.log(myarr2[2])

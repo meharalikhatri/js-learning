7d77a2b266716a9ec4cc68c88c6b0980c23e4fc3
meharalikhatri <56135432+meharalikhatri@users.noreply.github.com>
2024-03-23 18:01:29 +0000

    math

---
+++ 01_basics_java/numbers_maths.js
@@ -0,0 +1,20 @@
+//use inspect elements for further info
+
+//numbers
+let score = 4000000
+let balance = new Number(599)
+
+// console.log(score)
+// console.log(balance.toString().length) /* numbers will convert into string and then further you check it length, prototype */
+// console.log(balance.toFixed(2)); /* decimal */
+// console.log(score.toLocaleString()) /* comma seprated*/
+
+//maths
+
+// console.log(Math);
+// console.log(Math.abs(-1));
+// console.log(Math.min(3,5));
+// console.log(Math.floor(4.5));
+// console.log(Math.ceil(5.1));
+
+console.log(Math.random())

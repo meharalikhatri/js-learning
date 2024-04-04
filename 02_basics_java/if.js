c70402bca86541950954ca03cdadb52850c4d98d
meharalikhatri <56135432+meharalikhatri@users.noreply.github.com>
2024-04-04 07:36:51 +0000

    if

---
+++ 02_basics_java/if.js
@@ -0,0 +1,13 @@
+//Control Flow
+//You will not every time run entire code.
+
+//Bitwise Operators
+// And = && (empathises)
+//Or = || (Pipesign)
+
+const islogged = true
+if (islogged = true) {
+    console.log("user is logged");
+} else {
+    console.log("user not logged in")
+}

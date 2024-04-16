01740cf9fd4d76c455e4b6d35820914f37be1dd3
meharalikhatri <56135432+meharalikhatri@users.noreply.github.com>
2024-04-16 07:48:19 +0000

    map update

--- 01_basics_java/test.js
+++ 01_basics_java/test.js
@@ -10,10 +10,32 @@ let myObject = {
 const myData = [
     {
         driver_id : "abcdefg",
-        balance : 400
+        balance : 400,
+        registration : 2012
     },
     {
-        driver_id : "pqrstuv",
-        balance : -300
-    }
+        driver_id : "424dfae",
+        balance : 212,
+        registration : 2013
+    },
+    {
+        driver_id : "abcderqer",
+        balance : -120,
+        registration : 2012
+    },
+    {
+        driver_id : "abcdfaefeefg",
+        balance : 80,
+        registration : 2013
+    },
+    {
+        driver_id : "abteedcdefg",
+        balance : 4001,
+        registration : 2013
+    },
 ]
+
+
+let finder = myData.filter( (reg) => reg.registration == 2013 && reg.balance < 1000 )
+
+console.log(finder);
--- 02_basics_java/filter.js
+++ 02_basics_java/filter.js
@@ -28,4 +28,39 @@ const myvalue = [1,3,5,7,9]
 
 const yourvalue = myvalue.filter( (num) => num > 5 )
 
-console.log(yourvalue);
+// console.log(yourvalue);
+
+
+
+const myData = [
+    {
+        driver_id : "abcdefg",
+        balance : 400,
+        registration : 2012
+    },
+    {
+        driver_id : "424dfae",
+        balance : 212,
+        registration : 2013
+    },
+    {
+        driver_id : "abcderqer",
+        balance : -120,
+        registration : 2012
+    },
+    {
+        driver_id : "abcdfaefeefg",
+        balance : 80,
+        registration : 2013
+    },
+    {
+        driver_id : "abteedcdefg",
+        balance : 4001,
+        registration : 2013
+    },
+]
+
+
+let finder = myData.filter( (reg) => reg.registration == 2013 && reg.balance < 1000 )
+
+console.log(finder);

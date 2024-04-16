415327897deb97b397a072222e95fbe35918943a
meharalikhatri <56135432+meharalikhatri@users.noreply.github.com>
2024-04-16 06:36:55 +0000

    update test

--- 01_basics_java/test.js
+++ 01_basics_java/test.js
@@ -1,1 +1,19 @@
-
+const myNum = [1,3,5,7,9]
+let myAlph = ["a","b","c","d"]
+let empAlph = []
+let name = "ali"
+let age = 40
+let myObject = {
+    name : "ali khatri",
+    phone : 3315900000
+}
+const myData = [
+    {
+        driver_id : "abcdefg",
+        balance : 400
+    },
+    {
+        driver_id : "pqrstuv",
+        balance : -300
+    }
+]
